a9924568132bf825554b18ebe911f05df2c56a05
finkangjung <121650445+finkangjung@users.noreply.github.com>
2023-01-05 14:14:19 +0900

    커멘츠 뷰파일 컨트롤러 수정

--- SeeREAL/src/main/java/com/kh/seeReal/comments/controller/CommentsController.java
+++ SeeREAL/src/main/java/com/kh/seeReal/comments/controller/CommentsController.java
@@ -1,11 +1,30 @@
 package com.kh.seeReal.comments.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
 import com.kh.seeReal.comments.model.service.CommentsService;
+
+
+
 
 @Controller
 public class CommentsController {
@@ -21,4 +40,108 @@ public class CommentsController {
 		
 		return "movieMain";
 	}
+	
+	
+	@RequestMapping(value="movieDetail.co")
+	public String detailMovie(String movieDate,String movieTitle,String movieImg,Model model) {
+		
+		System.out.println(movieDate);
+		System.out.println(movieTitle);
+		System.out.println(movieImg);
+		
+		model.addAttribute("movieDate",movieDate);
+		model.addAttribute("movieTitle",movieTitle);
+		model.addAttribute("movieImg",movieImg);
+		
+		return "comments/movieDetail";
+		
+	}
+	
+	
+	
+	@ResponseBody
+	@RequestMapping(value = "movie.do", produces="application/json; charset=UTF-8")
+	public String searchMovie(String title) throws IOException {
+		//String[] mvList = box();
+		
+		String clientId = "Uw8Fe7ZNBoyhy9E3Qn2R"; //애플리케이션 클라이언트 아이디 필수작성
+        String clientSecret = "XoR_59jjvJ"; //애플리케이션 클라이언트 시크릿 필수작성
+        
+        String text = URLEncoder.encode(title, "UTF-8");
+        
+        String responseBody = "";
+        /*
+        JSONArray jarr = new JSONArray();
+        for(int i = 0; i < mvList.length; i++) {
+	        String apiURL = "https://openapi.naver.com/v1/search/movie.json?query=" + URLEncoder.encode(mvList[i], "UTF-8");
+	        Map<String, String> requestHeaders = new HashMap<>();
+	        requestHeaders.put("X-Naver-Client-Id", clientId);
+	        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+	        //responseBody += get(apiURL,requestHeaders);
+	        jarr.add(get(apiURL,requestHeaders));
+        }
+        */
+        
+        String apiURL = "https://openapi.naver.com/v1/search/movie.json?query=" + URLEncoder.encode(title, "UTF-8");
+        // apiURL += "&display=100"; (한페이지에 보여줄 개수) 
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+        responseBody = get(apiURL,requestHeaders);
+        System.out.println(responseBody);
+        //System.out.println(jarr);
+        return responseBody;
+	}
+	
+	private static String get(String apiUrl, Map<String, String> requestHeaders){
+        HttpURLConnection con = connect(apiUrl);
+        try {
+            con.setRequestMethod("GET");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                return readBody(con.getInputStream());
+            } else { // 오류 발생
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+            System.out.println(responseBody.toString());
+            
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
+        }
+    }
 }
--- SeeREAL/src/main/webapp/WEB-INF/views/comments/movieMain.jsp
+++ SeeREAL/src/main/webapp/WEB-INF/views/comments/movieMain.jsp
@@ -4,9 +4,103 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>영화 검색?</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<style>
+	a{
+		text-decoration : none;
+		color : black;
+	}
+	
+</style>
 </head>
 <body>
+
+	<input placeholder="영화제목을 입력하세요~" id="title" /><button onclick="movie();">이동~</button>
+	
+	
+		<table id="result1" border="1" align="center">
+		<thead>
+			<tr>
+				<th>영화제목(링크)</th>
+				<th>이미지</th>
+				<th>개봉일</th>
+				<th>감독</th>
+				<th>출연배우</th>
+				<th>평점</th>
+			</tr>
+		</thead>
+		<tbody>
+		</tbody>
+	</table>
+	<form action="movieDetail.co" method="post" id="form1">
+		<input type="hidden" id="movieDate" name="movieDate">
+		<input type="hidden" id="movieTitle" name="movieTitle">
+		<input type="hidden" id="movieImg" name="movieImg">
+		
+	</form>
+	<input type="text" id="test1">
+	
+	<script>
+		function movie(){
+			$.ajax({
+				url : 'movieTest1.do',
+				data : {title : $('#title').val()},
+				success : data => {
+					console.log(data.items[0]);
+					//console.log(data.getFoodKr.item);
+					
+					
+					
+					const itemArr = data.items;
+					
+					let value = '';
+					for(let i in itemArr){
+						let item = itemArr[i];
+						console.log(item);
+						let thumb = item.image;
+						
+						value += '<tr>'
+							   + '<td><a href="'+ item.link + '">' + item.title + '</a></td>'
+							   + '<td><img src="' + thumb + '"/></td>'
+							   + '<td name="movieDate">' + item.pubDate + '</td>'
+							   + '<td>' + item.director + '</td>'
+							   + '<td>' + item.actor + '</td>'
+							   + '<td>' + item.userRating + '</td>'
+							   + '</tr>' 
+					}
+					
+					
+					$('#result1 tbody').html(value);
+					console.log(value);
+					$('#title').val(title);
+					console.log($('#result1 tbody').children())
+					
+				},
+				error : () => {
+					console.log('요건조금...');
+				}
+				
+			});	
+			console.log($('#result1 tbody'))
+			console.log('---1')
+			$(document).on('click','#result1 tbody>tr',function(){
+				console.log($('#result1 tbody>tr'));
+				console.log(this)
+				console.log('--x--')
+				console.log($(this).children().eq(0))
+				console.log($(this).children().children().eq(1).attr('src'))
+				
+				$('#movieTitle').val($(this).children().eq(0).text());
+				$('#movieDate').val($(this).children().eq(2).text());
+				$('#movieImg').val($(this).children().children().eq(1).attr('src'));
+				$('#form1').submit();
+			})
+		}
+	</script>
+	
+
+	
 
 </body>
 </html>
