9dd9d3ace1c20ec4b3531c53de6b33de027b7428
YEJINK072 <121650317+YEJINK072@users.noreply.github.com>
2023-01-05 15:55:04 +0900

    feed 수정 /예진

--- SeeREAL/src/main/java/com/kh/seeReal/member/model/dao/FeedDao.java
+++ SeeREAL/src/main/java/com/kh/seeReal/member/model/dao/FeedDao.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class FeedDao {
-	public int selectCommentsCount(SqlSessionTemplate sqlSession) {
+	public int selectFeed(SqlSessionTemplate sqlSession) {
 		return sqlSession.selectOne("feedMapper.selectCommentsCount");
 	}
 }
--- SeeREAL/src/main/java/com/kh/seeReal/member/model/service/FeedService.java
+++ SeeREAL/src/main/java/com/kh/seeReal/member/model/service/FeedService.java
@@ -1,10 +1,7 @@
 package com.kh.seeReal.member.model.service;
 
 public interface FeedService {
-
-	// 평가수
-	int selectCommentsCount();
-	// 별점분포
 	
-	// 리얼평 영화제목/별점/좋아요수
+	int selectFeed();
+	
 }
