8ddb826afb629f0b8fe6e5d5d848ceae50e45c08
bear4231 <121650462+bear4231@users.noreply.github.com>
2023-01-05 09:51:44 +0900

    BoardService, BoardDao 생성 - 이충원 -

---
+++ SeeREAL/src/main/java/com/kh/seeReal/board/model/service/BoardService.java
@@ -0,0 +1,5 @@
+package com.kh.seeReal.board.model.service;
+
+public interface BoardService {
+
+}
---
+++ SeeREAL/src/main/java/com/kh/seeReal/board/model/service/BoardServiceImpl.java
@@ -0,0 +1,5 @@
+package com.kh.seeReal.board.model.service;
+
+public class BoardServiceImpl {
+
+}
