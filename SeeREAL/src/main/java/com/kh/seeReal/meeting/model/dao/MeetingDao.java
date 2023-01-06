4aa31de2e1ef9975de1f836834b567ce4da7a9d8
F0GB0W <97530704+F0GB0W@users.noreply.github.com>
2023-01-06 10:18:52 +0900

    모임 리스트 추가 - 주은

--- SeeREAL/src/main/java/com/kh/seeReal/meeting/model/dao/MeetingDao.java
+++ SeeREAL/src/main/java/com/kh/seeReal/meeting/model/dao/MeetingDao.java
@@ -1,8 +1,12 @@
 package com.kh.seeReal.meeting.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.seeReal.common.model.vo.PageInfo;
 import com.kh.seeReal.meeting.model.vo.Meeting;
 
 @Repository
@@ -10,6 +14,19 @@ public class MeetingDao {
 
 	public int insertMeeting(SqlSessionTemplate sqlSession, Meeting meeting) {
 		return sqlSession.insert("meetingMapper.insertMeeting", meeting);
+	}
+
+	public int selectMeetingListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("meetingMapper.selectMeetingListCount");
+	}
+
+	public ArrayList<Meeting> selectMeetingList(SqlSessionTemplate sqlSession, PageInfo pi) {
+
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+		
+		return (ArrayList)sqlSession.selectList("meetingMapper.selectMeetingList", pi, rowBounds);
 	}
 
 }
--- SeeREAL/src/main/java/com/kh/seeReal/meeting/model/service/MeetingService.java
+++ SeeREAL/src/main/java/com/kh/seeReal/meeting/model/service/MeetingService.java
@@ -1,8 +1,15 @@
 package com.kh.seeReal.meeting.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.seeReal.common.model.vo.PageInfo;
 import com.kh.seeReal.meeting.model.vo.Meeting;
 
 public interface MeetingService {
 
 	int insertMeeting(Meeting meeting);
+
+	int selectMeetingListCount();
+
+	ArrayList<Meeting> selectMeetingList(PageInfo pi);
 }
