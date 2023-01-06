f672163dd92c9f8c697c97dc5b11d361e97a6480
bear4231 <121650462+bear4231@users.noreply.github.com>
2023-01-06 09:31:27 +0900

    중간 save -이충원-

--- SeeREAL/src/main/java/com/kh/seeReal/board/controller/BoardController.java
+++ SeeREAL/src/main/java/com/kh/seeReal/board/controller/BoardController.java
@@ -1,5 +1,10 @@
 package com.kh.seeReal.board.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,16 +46,36 @@ public class BoardController {
 	 public String insertSopiler(Board b, MultipartFile upfile, HttpSession session, Model model ) {
 		 
 		 if(!upfile.getOriginalFilename().equals("")) {
+			
 			 b.setOriginName(upfile.getOriginalFilename());
+			 b.setChangeName("resources/uploadFiles/" + saveFile(upfile, session));
 		 }
 		 
 		 
 		 
-		 boardService.insertSpoiler(b);
-		 return "main";
+		 if(boardService.insertSpoiler(b) > 0) {
+			 return "redirect:/spoilerList.bo";
+		 }else {
+			 model.addAttribute("errorMsg", "글 등록 실패");
+			 return "common/errorPage";
+		 }
 	 }
-	
-	
-	
-	
+	 
+	 public String saveFile(MultipartFile upfile, HttpSession session) {
+		 
+		 String originName = upfile.getOriginalFilename();
+		 String currentTime = new SimpleDateFormat("yyyyMMddHHss").format(new Date());
+		 int ranNum = (int)(Math.random() * 90000 + 10000);
+		 String ext = originName.substring(originName.lastIndexOf("."));
+		 String changeName = currentTime + ranNum + ext;
+		 String savePath = session.getServletContext().getRealPath("/resources/uploadFiles/");
+		 
+		 try {
+			upfile.transferTo(new File(savePath + changeName));
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+		}
+		 return changeName;
+	 
+	 }
 }
