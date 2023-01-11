df8be1612a97344d7a0416f6b02dc4cd2e2e26d9
F0GB0W <97530704+F0GB0W@users.noreply.github.com>
2023-01-11 15:46:11 +0900

    컬렉션 만들기 완성 및 영화 검색 오류 수정 -주은

---
+++ SeeREAL/src/main/java/com/kh/seeReal/collection/model/service/CollectionService.java
@@ -0,0 +1,18 @@
+package com.kh.seeReal.collection.model.service;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.kh.seeReal.collection.model.vo.Collection;
+import com.kh.seeReal.collection.model.vo.CollectionMovie;
+import com.kh.seeReal.collection.model.vo.CollectionMovieList;
+
+public interface CollectionService {
+
+	int insertCollection(Collection collection);
+
+	int insertCollectionMovie(Map<String, Object> map);
+
+	int insertCollectionMovie(CollectionMovieList movieList);
+
+}
